40c413b93d875d9e48a7264250298d760a74e7bd
hp7hao <hp7hao@outlook.com>
2025-04-12 20:34:01 +0800

    add mpv api

--- libs/pico8/pico8go.lua
+++ libs/pico8/pico8go.lua
@@ -2,3 +2,19 @@ function vibrate(s,d)
     printh("vibrate "..s.." "..d,"vibrator")
 end
 
+function sfxplay(s)
+    printh("mpv:stop","pico8goapi")
+    printh("mpv:play "..s,"pico8goapi")
+end
+
+function sfxstop()
+    printh("mpv:stop","pico8goapi")
+end
+
+function sfxpause()
+    printh("mpv:pause","pico8goapi")
+end
+
+function sfxresume()
+    printh("mpv:resume","pico8goapi")
+end
