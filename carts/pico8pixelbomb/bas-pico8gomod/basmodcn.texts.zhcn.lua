f63c3f886707f6aa2669ae3ecddb11866503370d
hp7hao <hp7hao@outlook.com>
2025-04-18 07:51:07 +0800

    add rumble and chinese in BAS

---
+++ carts/pico8pixelbomb/bas-pico8gomod/basmodcn.texts.zhcn.lua
@@ -0,0 +1,6 @@
+i18n.texts["zhcn"]={}
+i18n.texts["zhcn"]["best"]="17c44fe525af41a00110fe2838feaaba001282844be1424cc00"
+i18n.texts["zhcn"]["game over"]="1a87eae32d49eb40010ae83ea84a44aa00148be489cc01cdc00110fe547c1038d600"
+i18n.texts["zhcn"]["highscore"]="17c44fe525af41a00110fe2838feaaba001282844be1424cc00"
+i18n.texts["zhcn"]["new"]="122fc582efa72aa0015e82429ed012de0017c04fe5438d63000"
+i18n.texts["zhcn"]["score"]="15c947ec47e544c001282844be1424cc00"
